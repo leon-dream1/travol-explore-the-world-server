a5382842a1dbfd948a4feb12a4bb818acbcb4e3b
leon-dream1~ <nahidulislam019045@gmail.com>
2024-04-28 01:59:36 +0600

    Tourist added to DB

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
-const port = process.env.PORT ||  5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
@@ -11,45 +11,51 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l574mko.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-
 const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 //   app.get('/', (req, res) => {
 //     res.send('Hello World!')
 //   })
 
-  async function run() {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-    //   await client.connect();
-      // Send a ping to confirm a successful connection
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+    // Send a ping to confirm a successful connection
     //   await client.db("admin").command({ ping: 1 });
 
-    app.get('/', (req, res) => {
-        res.send('Hello From MongoDB!')
-      })
+    app.get("/", (req, res) => {
+      res.send("Hello From MongoDB!");
+    });
+
+    const touristSpotCollection = client.db("tourManageMentDb").collection('touristSpot');
 
 
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    app.post('/touristSpot', async (req, res) => {
+      const spot = req.body;
+      console.log(spot);
+      const result = await touristSpotCollection.insertOne(spot);
+      res.send(result);
+    })
 
-
-    } finally {
-      // Ensures that the client will close when you finish/error
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
     //   await client.close();
-    }
   }
-  run().catch(console.dir);
-
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
