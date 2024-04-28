ffed53a6aa2f90b6da2c492dc6fe9de0c5333b4e
leon-dream1~ <nahidulislam019045@gmail.com>
2024-04-28 19:50:41 +0600

    Delete spot done

--- index.js
+++ index.js
@@ -111,6 +111,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/touristSpot/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await touristSpotCollection.deleteOne(query);
+      res.send(result);
+    });
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
