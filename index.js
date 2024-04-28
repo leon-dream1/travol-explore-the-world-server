f455bef6e4b4d563434390ece21b7a49115de69b
leon-dream1~ <nahidulislam019045@gmail.com>
2024-04-28 15:05:09 +0600

    All tourist Spot and user tourist spot added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
@@ -28,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
+    await client.connect();
     // Send a ping to confirm a successful connection
     //   await client.db("admin").command({ ping: 1 });
 
@@ -36,15 +36,44 @@ async function run() {
       res.send("Hello From MongoDB!");
     });
 
-    const touristSpotCollection = client.db("tourManageMentDb").collection('touristSpot');
+    const touristSpotCollection = client
+      .db("tourManageMentDb")
+      .collection("touristSpot");
 
+    app.get("/allTouristSpot", async (req, res) => {
+      const cursor = touristSpotCollection.find({});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    app.post('/touristSpot', async (req, res) => {
+    app.get("/touristSpot", async (req, res) => {
+      const cursor = touristSpotCollection.find({});
+      const result = await cursor.toArray();
+      res.send(result.slice(0, 6));
+    });
+
+    app.get("/touristSpot/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await touristSpotCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/myTouristSpot/:email", async (req, res) => {
+      const email = req.params.email;
+      console.log(email);
+      const query = { email: email };
+      const cursor = touristSpotCollection.find(query);
+      const result = await cursor.toArray(); 
+      res.send(result);
+    });
+
+    app.post("/touristSpot", async (req, res) => {
       const spot = req.body;
-      console.log(spot);
       const result = await touristSpotCollection.insertOne(spot);
       res.send(result);
-    })
+    });
 
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
